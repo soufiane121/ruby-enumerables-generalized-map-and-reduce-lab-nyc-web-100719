07cbef7a416909657d3e0d13cd0d30e2ad42155e
soufiane <soufiane.oucherrou@gmail.com>
2019-09-20 02:47:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,10 +4,15 @@ end
 
 
 def reduce(arg, optional = nil)
-optional_arg = optional ? optional : arg[0]
-arg.reduce(optional_arg) do |acu, ele|
-  acu = yield(acu, ele)
-  acu-
+if optional
+  optional_arg = optional
+  i=0
+else
+optional_arg = arg[0]  
+i=1 
 end
-
+while i <arg.length
+optional_arg = yield(optional_arg,arg[i])
+i+=1
+end
 end
