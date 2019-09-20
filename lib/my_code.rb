ebb583095c6babdea257a7b81695e0344a221b9e
soufiane <soufiane.oucherrou@gmail.com>
2019-09-20 03:33:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(arg)
+arg.map { |ele| yield(ele)}
+end
+
+
+def reduce(arg, optional = nil)
+if optional
+  optional_arg = optional
+  i=0
+else
+optional_arg = arg[0]  
+i=1 
+end
+while i <arg.length
+optional_arg = yield(optional_arg,arg[i])
+i+=1
+
+end
+optional_arg
+end
+
+
+
+    
+    
